018842da64e7b9efb668e9bc8cdf8f55a8852181
John Besco <johnbesco.dev@gmail.com>
2023-01-13 23:53:32 +0000

    Complete Tasks

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const splitString = string.split("");
+    const reverseArray = splitString.reverse();
+    const joinArray = reverseArray.join("");
+    return joinArray;
 };
+
+reverseString("")
 
 // Do not edit below this line
 module.exports = reverseString;
