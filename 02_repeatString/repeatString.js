8d7c4e159a50faf9d7dc72eabacb8dc166d7919e
John Besco <johnbesco.dev@gmail.com>
2023-01-13 23:42:24 +0000

    Complete tasks

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,12 +1,17 @@
+let num = Math.random() * 1000;
+
 const repeatString = function(string, num) {
     let repeatedString = '';
+    if (num < 0) {
+        return 'ERROR';
+    }
     while (num > 0) {
         repeatedString += string;
         num--;
     }
     return repeatedString;
 };
-repeatString('hey', 1);
+repeatString('', num);
 
 // Do not edit below this line
 module.exports = repeatString;
