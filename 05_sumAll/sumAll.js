95e93313f7fc00e7d6c5e712ee0664e3bf13a228
John Besco <johnbesco.dev@gmail.com>
2023-01-17 00:02:03 +0000

    Complete tasks

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
 
+    if (!Number.isInteger(start) || !Number.isInteger(end)) {
+        return 'ERROR';
+    } else if (start < 0 || end < 0) {
+        return 'ERROR';
+    } else if (start > 1) {
+        for (let i = 0; i <= start; i++) {
+            sum += i;
+        }
+    } else {
+        for (let i = 0; i <= end; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
+
+sumAll(10, "90")
 
 // Do not edit below this line
 module.exports = sumAll;
