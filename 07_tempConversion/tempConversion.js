dab447c0b277552ea43e39521d0b2e012962b249
John Besco <johnbesco.dev@gmail.com>
2023-01-19 23:15:46 +0000

    Complete Tasks

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+let fahrenheit = -100;
+
+const convertToCelsius = function(fahrenheit) {
+  return Math.round(((fahrenheit - 32) * 5/9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+let celsius = -10;
+
+const convertToFahrenheit = function(celsius) {
+  return Math.round(((9/5) * celsius + 32) * 10) / 10;
 };
 
 // Do not edit below this line
