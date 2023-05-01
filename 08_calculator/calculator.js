1437fae4eb1fae2b2148e773bf2afa37f4fb7db3
John Besco <johnbesco.dev@gmail.com>
2023-05-01 23:08:04 +0100

    Complete tasks

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -25,8 +25,12 @@ const power = function(x, y) {
 
 
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number === 0) {
+    return 1;
+  } else {
+    return number * factorial ( number - 1);
+  };
 };
 
 // Do not edit below this line
