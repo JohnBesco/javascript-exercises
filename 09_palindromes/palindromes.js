168a18ed7c371cbb57713a7c5e06d208ae7622f0
John Besco <johnbesco.dev@gmail.com>
2023-05-02 10:21:55 +0100

    Compete tasks

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+let string = ('A car, a man, a maraca.');
+const palindromes = function(string) {
+    const newString = string.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~() ]/g, "");
+    const reversed = newString.split("").reverse().join("");
+    if (reversed === newString) {
+        return true;
+    } else {
+        return false;
+    };
 };
 
 // Do not edit below this line
