b7728a0fca31dfe2f816fedddc43d07694794516
John Besco <johnbesco.dev@gmail.com>
2023-05-03 08:50:54 +0100

    Finish tasks

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+function getAge(person) {
+    const currentYear = new Date().getFullYear();
+    const age = person.yearOfDeath ? person.yearOfDeath - person.yearOfBirth : currentYear - person.yearOfBirth;
+    return age;
+}
 
+const findTheOldest = function(people) {
+    return people.reduce((oldestPerson, currentPerson) => {
+        const oldestAge = getAge(oldestPerson);
+        const currentAge = getAge(currentPerson);
+        return currentAge > oldestAge ? currentPerson : oldestPerson;
+    });
 };
+
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
